b8259c9c56e6c5e9cd323de59f395d88570e6d47
mrnvr <marin.virlogeux@gmail.com>
2018-03-28 08:34:31 +0200

    test cookies

--- api/authentication/check_auth.js
+++ api/authentication/check_auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 // const cookiename = 'usercookie'
 
 module.exports = (req, res, next) => {
+  console.log(req.cookies)
   try {
     const token = req.cookies.usercookie
     req.userData = jwt.verify(token, process.env.TOKEN_KEY)
