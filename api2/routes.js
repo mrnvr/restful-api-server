059f65c7c9af728dd40d92643646686162c301ed
drissireda <mohamedreda.drissi@edu.uca.ma>
2018-03-28 12:23:45 +0200

    modified totp routes and their dependencies

--- api2/routes.js
+++ api2/routes.js
@@ -1,3 +1,5 @@
+const passport=require('passport')
+const TotpStrategy= require('passport-totp').Strategy
 router.get('/totp-setup', function(req, res){
 
   const secret = base32.encode(crypto.randomBytes(16)).toString().replace(/=/g, '')
@@ -10,7 +12,30 @@ router.get('/totp-setup', function(req, res){
     url : url
   })
 })
+passport.use(new TotpStrategy( function(user, done){
+  console.log("this is totp******************************")
+  if(!user.key){
+    return done(new Error('no key'))
+  } else {
+    return done(null, base32.decode(user.key), 30)
+  }
+})
 router.get('/totp-input', function(req,res){
-  const userId = jwt.verify(req.cookies['usercookie'].token, process.env.TOKEN_KEY)
-
+  const cookie = jwt.verify(req.cookies['usercookie'].token, process.env.TOKEN_KEY)
+  User.findOne({_id : cookie.userId}, function(err, user){
+    if(!user.key)
+      user.key=cookie.key
+    passport.authenticate('totp', function(err, user, info){
+      if(err){
+        res.status(403).json({error : err})
+      } else {
+      user.save(function(err){
+        if(err)
+          console.log('problem') console.log(err)
+        else
+          console.log('updated key successefully')
+      })
+      res.status(200).json({message : "authentified"})
+    })
+  })
 })
